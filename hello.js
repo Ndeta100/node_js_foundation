b471b98da341cc7454f008a3e08699b53699381d
Ndeta Innocent <you@example.com>
2022-06-12 18:36:16 +0300

    using http

--- hello.js
+++ hello.js
@@ -8,3 +8,34 @@ if(mission==='Learn'){
 }else{
     console.log(`Is ${mission}ing really more fun?`)
 }
+const EvenEmitter=require('events')
+class MyEmitter extends EvenEmitter {
+
+}
+//Subscribe to celebrity for Observer one
+const celebrity=new MyEmitter()
+celebrity.on('race win',()=>{
+    console.log('Congrats, you are the best')
+})
+//Subscribe to celebrity for Observer one
+celebrity.on('race win',()=>{
+    console.log('Boo I could have better than that')
+})
+//using the even on our process function
+process.on('exit',(code)=>{
+ console.log(`Process exit with code : ${code}`)
+})
+celebrity.emit('race win')
+
+
+//MAKING HTTP REQUESTS
+const http=require('http')
+const req=http.request('http://ww.google.com',(res)=>{
+    res.on('data',(chunk)=>{
+        console.log(`Data chunk: ${chunk}`)
+    })
+    res.on('end',()=>{
+        console.log('No more data')
+    })
+})
+req.end()
---
+++ readme.md
@@ -0,0 +1,5 @@
+## Uderstanding the internals of Node js and how it works REVISITED
+
+     * Libuv
+     * Node js API's 
+     * Javascript V8 engine
